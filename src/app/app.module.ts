67a0285f8b45a39940f76be02493cc7881243d8f
Adel Acebal <adelacebal@yahoo.com>
2017-06-24 12:56:18 -0400

    quote item added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { CountersComponent } from './counters/counters.component';
 import { ChuckNorrisPageComponent } from './chuck-norris-page/chuck-norris-page.component';
+import { QuoteItemComponent } from './quote-item/quote-item.component';
 
 const routes: Routes = [
   // http://localhost:4200/quotes
@@ -46,7 +47,8 @@ const routes: Routes = [
     ContactListComponent,
     ContactComponent,
     CountersComponent,
-    ChuckNorrisPageComponent
+    ChuckNorrisPageComponent,
+    QuoteItemComponent
   ],
   imports: [
     BrowserModule,
