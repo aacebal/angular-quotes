e861a44aeaef7e650a3d2c535aae74d50d8c3e0a
Adel Acebal <adelacebal@yahoo.com>
2017-06-24 11:46:06 -0400

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { CountersComponent } from './counters/counters.component';
+import { ChuckNorrisPageComponent } from './chuck-norris-page/chuck-norris-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteListComponent,
+    MyHomeComponent,
+    MyAboutComponent,
+    ContactListComponent,
+    ContactComponent,
+    CountersComponent,
+    ChuckNorrisPageComponent
   ],
   imports: [
     BrowserModule
